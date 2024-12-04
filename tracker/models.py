a41acdf1a1658c90866a827cba5262c590471f2e
Bethan Cottell <cottellbc@gmail.com>
2024-12-04 10:22:47 +0000

    migrations made

--- tracker/models.py
+++ tracker/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 
 class custom_user(models.Model):
     username = models.CharField(max_length=50, unique = True)
-    firstname = models.Charfield(max_length=30)
-    lastname = models.Charfield(max_length=30)
-    email = models.Emailfield(unique = True)
-    password = models.CharField(_('password'), max_length=128)
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.EmailField(unique = True)
+    password = models.CharField(('password'), max_length=128)
     is_staff = models.BooleanField(default = False)
 
     REQUIRED_FIELDS = ['username', 'firstname', 'lastname', 'email', 'password']
